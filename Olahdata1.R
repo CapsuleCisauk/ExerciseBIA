98f591dc46c38e082bdefe618fe7269459624a12
Rifkiyan Nur Cahyo <rifkiyancahyoadi@gmail.com>
2024-11-08 07:21:54 +0700

    Add files via upload
    Here my assignment of BIA

---
+++ Olahdata1.R
@@ -0,0 +1,5 @@
+# Load the dataset
+url <- "https://raw.githubusercontent.com/CapsuleCisauk/ExerciseBIA/refs/heads/main/doordash_restaurants2.csv"
+data <- read.csv(url)
+# View the first few rows of the dataset to confirm it's loaded
+head(data)
---
+++ Olahdata2.R
@@ -0,0 +1,5 @@
+# Load the dataset
+url <- "https://raw.githubusercontent.com/CapsuleCisauk/ExerciseBIA/refs/heads/main/doordash_restaurants2.csv"
+data <- read.csv(url)
+# View a few rows of the dataset to confirm it's loaded
+View(data)
